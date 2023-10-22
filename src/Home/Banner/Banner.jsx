f10b57f34aa0e7d45ad1cae3cf808b3da1014c3c
Tomal Ahmed Raj <sj.tomalahmedraj@gmail.com>
2023-10-22 18:24:30 +0600

    new

--- src/Home/Banner/Banner.jsx
+++ src/Home/Banner/Banner.jsx
@@ -3,7 +3,14 @@ import React from 'react'
 const Banner = () => {
   return (
     <>
-    hi
+    <div className="container mx-auto">
+      <div class="grid grid-cols-3 gap-8 sm:px-6 lg:px-8">
+        <div class="col-span-3 md:col-span-1 lg:col-span-1 xl:col-span-1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi ullam unde delectus ex.</div>
+        <div class="col-span-3 md:col-span-1 lg:col-span-1 xl:col-span-1">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut ex repellat ipsum labore ducimus officia?</div>
+        <div class="col-span-3 md:col-span-1 lg:col-span-1 xl:col-span-1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni nemo nisi architecto? Temporibus, nostrum eaque aperiam molestiae quas porro.</div>
+      </div>
+    </div>
+      <h1 className='text-[40px]'>Hi</h1>
     </>
   )
 }
--- src/Home/Navbar/Navber.jsx
+++ src/Home/Navbar/Navber.jsx
@@ -20,8 +20,8 @@ export default function Navber() {
     <Disclosure as="nav" className="navber-style bg-light-800">
       {({ open }) => (
         <>
-          <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-            <div className="relative -16 flex items-center justify-between sm:h-16">
+          <div className="mx-auto container   sm:px-6 lg:px-8">
+            <div className="relative -16 flex items-center justify-between">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                 {/* Mobile menu button*/}
                 <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
